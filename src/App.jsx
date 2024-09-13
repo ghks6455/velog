dbd7ae5a13ec9277dbfb4712d656bd568a83dfc3
ghks6455 <ghks6455@gmail.com>
2024-09-13 17:09:19 +0900

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import postData from "./js/DUMMY_DATA";
+import InputForm from "./components/InputForm";
 
 function App() {
+  const [showCard, setShowCard] = useState(true);
   const [data, setData] = useState("trending");
+  const [velogContents, setVelogContents] = useState(postData);
+
+  const list = velogContents;
 
   return (
     <>
-      <Header setData={setData} data={data} />
+      <Header
+        setData={setData}
+        data={data}
+        setShowCard={setShowCard}
+        showCard={showCard}
+      />
+
       <main>
-        {postData[data].map((postData) => {
-          return <Card key={postData.id} postData={postData} />;
-        })}
+        {showCard ? (
+          list[data].map((post) => <Card key={post.id} postData={post} />)
+        ) : (
+          <InputForm setVelogContents={setVelogContents} />
+        )}
       </main>
     </>
   );
